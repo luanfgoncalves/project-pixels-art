27da7911e56264b6e22ce7f62243ab6246e8bffa
luanfgoncalves <luanfgoncalves@outlook.com>
2022-04-25 12:10:22 -0300

    finalização do html e maior parte do css

---
+++ script.js
@@ -0,0 +1,4 @@
+const pixelBoard = document.getElementById('pixel-board');
+const boardSize = document.getElementById('board-size');
+const pixels = document.getElementsByClassName('pixel');
+const colors = document.getElementsByClassName('color');
