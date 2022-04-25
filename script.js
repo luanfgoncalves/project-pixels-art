2f35402404d0e3957c5cb3c784cf45765b0d705f
luanfgoncalves <luanfgoncalves@outlook.com>
2022-04-25 16:31:47 -0300

    Correção do titulo + pequenas correções do EsLint

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//elementos
+// elementos
 
 const pixelBoard = document.getElementById('pixel-board');
 const boardSize = document.getElementById('board-size');
-const pixels = document.getElementsByClassName('.pixel');
+const pixels = document.getElementsByClassName('pixel');
 
 const colorPallet = document.getElementById('color-palette');
 const colors = document.getElementsByClassName('color');
@@ -10,12 +10,12 @@ const paintBlack = document.getElementById('paint-black');
 const paintRed = document.getElementById('paint-red');
 const paintBlue = document.getElementById('paint-blue');
 const paintGreen = document.getElementById('paint-green');
-const selectedColor = document.querySelector('selected')
+const selectedColor = document.querySelector('.selected');
 
 const eraser = document.getElementById('clear-board');
 const ereased = document.getElementsByClassName('pixel');
 
-//eventos e funções
+// eventos e funções
 
 colorPallet.addEventListener('click', function (event) {
   console.log('copiou');
@@ -48,7 +48,7 @@ pixelBoard.addEventListener('click', function (event) {
   }
 });
 
-eraser.addEventListener('click', function (event) {
+eraser.addEventListener('click', function () {
   console.log('apagou');
   for (let index = 0; index < eraser.length; index += 1) {
     eraser[index].style.backgroundColor = 'white';
