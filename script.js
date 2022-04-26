1fa40264bfcf2d1c98f877c04dc7a7d8060e2e4d
luanfgoncalves <luanfgoncalves@outlook.com>
2022-04-26 10:12:26 -0300

    correção das variaveis dentro de CliqueNoBotão, adição de checador de classe em CliqueNaPaleta e pequenas correções

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const pixelBoard = document.getElementById('pixel-board');
 // const boardSize = document.getElementById('board-size'); - guardar para o gerador de pixels
-const pixels = document.getElementsByClassName('pixel');
+
 const colorPallet = document.getElementById('color-palette');
 const colors = document.querySelectorAll('.color');
 const eraser = document.getElementById('clear-board');
@@ -21,42 +21,46 @@ colorPallet.addEventListener('click', function cliqueNaPaleta(event) {
   }
 });
 
-// botão de Limpar - Não funcionou
+// botão de Limpar - funcionou
 eraser.addEventListener('click', function cliqueNoBotao() {
+  const pixels = document.getElementsByClassName('pixel'); //precisa ser classe para gerar um array
   console.log('clique no botão reconhecido');
-  for (let index = 0; index < eraser.length; index += 1) {
+  for (let index = 0; index < pixels.length; index += 1) {
     pixels[index].style.backgroundColor = 'white';
   }
 });
 
-// seletor de cores - funcionou porém colore tudo que é div
+// seletor de cores - funcionou VQV
 pixelBoard.addEventListener('click', function cliqueNoQuadro(evt) {
   const paintBlack = document.getElementById('paint-black');
   const paintRed = document.getElementById('paint-red');
   const paintBlue = document.getElementById('paint-blue');
   const paintGreen = document.getElementById('paint-green');
   const selectedColor = document.querySelector('.selected');
+  const clickedPixel = evt.target;
+  const classChecker = evt.target.classList.contains('pixel');
   console.log('clique no quadro reconhecido');
-  if (selectedColor === paintBlack) {
-    const clickedPixel = evt.target;
+  if (selectedColor === paintBlack && classChecker === true) {
     clickedPixel.style.backgroundColor = 'black';
     console.log('cor preta aplicada');
   }
-  if (selectedColor === paintRed) {
-    const clickedPixel = evt.target;
+  if (selectedColor === paintRed && classChecker === true) {
     clickedPixel.style.backgroundColor = 'red';
     console.log('cor vermelha aplicada');
   }
-  if (selectedColor === paintBlue) {
-    const clickedPixel = evt.target;
+  if (selectedColor === paintBlue && classChecker === true) {
     clickedPixel.style.backgroundColor = 'blue';
     console.log('cor azul aplicada');
   }
-  if (selectedColor === paintGreen) {
-    const clickedPixel = evt.target;
+  if (selectedColor === paintGreen && classChecker === true) {
     clickedPixel.style.backgroundColor = 'green';
     console.log('cor verde aplicada');
   }
 });
 
 // gerador de pixeis - addicionar abaixo
+
+
+
+//Referencias:
+// https://bobbyhadz.com/blog/javascript-check-if-event-target-has-class#:~:text=To%20check%20if%20event.,target.
